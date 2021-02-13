2ff1e406a8ecf06cce508784ed4b204645f5aaf1
Tereza Dohnalova <xdohna10@mendelu.cz>
2021-02-13 19:52:24 +0100

    přídání šipky, úpravení JS funkce

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function ukazOdpoved(odpovedID) {
+function ukazOdpoved(odpovedID, sipkaID) {
     let odpovedDiv = document.getElementById(odpovedID);
-    if (odpovedDiv.style.display === "none") {
+    let sipka = document.getElementById(sipkaID);
+
+    if (sipka.src.match("img/arrow_down_white.svg")) {
       odpovedDiv.style.display = "block";
+      sipka.src = "img/arrow_up_white.svg";
+      console.log("prvni");
     } else {
       odpovedDiv.style.display = "none";
+      sipka.src = "img/arrow_down_white.svg";
+      console.log("druhy");
     }
+
   }
